8472deff843ee1952d6db79ab47067698176bed4
Etumnu Prosper <johnikechukwu94@gmail.com>
2022-08-29 16:22:48 +0100

    update

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,42 +8,47 @@ import { handlePostType, handleTaskId, showForm } from "../features/taskSlice";
 const Task = ({ task, index }) => {
   const dispatch = useDispatch();
   return (
-    <div key={index} className="p-3 border flex  md:w-[35%] w-[95%] ">
-      <img src="/me.jpg" className="w-10 h-10 rounded mr-2" alt="" />
-      {/* <img src={task?.image_url} alt="" /> */}
-      <div className="flex  justify-between ">
-        <div className="-mt-2">
-          <h4 className=" text-xs md:text-base">{task?.task_msg}</h4>
-          <p className="text-red-500 text-[8px] md:text-xs">
-            {task?.task_date}
-          </p>
+    <div
+      key={index}
+      className="p-3 border flex justify-between  md:w-[35%] w-[95%] "
+    >
+      <div className="flex">
+        <img src="/me.jpg" className="w-10 h-10 rounded mr-2" alt="" />
+        {/* <img src={task?.image_url} alt="" /> */}
+        <div className="flex ">
+          <div className="-mt-2">
+            <h4 className=" text-xs md:text-base">{task?.task_msg}</h4>
+            <p className="text-red-500 text-[8px] md:text-xs">
+              {task?.task_date}
+            </p>
+          </div>
         </div>
+      </div>
 
-        <div className="flex">
-          <div
+      <div className="flex">
+        <div
+          onClick={() => {
+            dispatch(handleTaskId(task.id));
+            dispatch(showForm(true));
+            dispatch(handlePostType());
+          }}
+          className=" hover:bg-slate-100 h-8  p-2 mr-3 border  flex rounded  "
+        >
+          <MdEdit />
+        </div>
+        <div className="rounded flex border h-8 ">
+          <span className="flex justify-center p-2  items-center">
+            <TbBellZ />
+          </span>
+          <span
             onClick={() => {
-              dispatch(handleTaskId(task.id));
               dispatch(showForm(true));
-              dispatch(handlePostType());
+              dispatch(handleTaskId(task.id));
             }}
-            className=" hover:bg-slate-50 p-[6px] mr-3 border  flex rounded justify-center items-center "
+            className="flex items-center justify-center p-2"
           >
-            <MdEdit />
-          </div>
-          <div className="rounded flex shadow">
-            <span className="flex justify-center p-[6px] items-center">
-              <TbBellZ />
-            </span>
-            <span
-              onClick={() => {
-                dispatch(showForm(true));
-                dispatch(handleTaskId(task.id));
-              }}
-              className="flex items-center justify-center p-[6px]"
-            >
-              <GrCheckmark />
-            </span>
-          </div>
+            <GrCheckmark />
+          </span>
         </div>
       </div>
     </div>
